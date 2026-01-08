9e5da2188d1fd7eaeaf57b1fc3ae13514c6bcfb4
JAVA <jayanandith123@gmail.com>
2026-01-08 12:10:07 +0530

    make the header and footer

---
+++ src/common/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className='bg-[#0F2854]'>
+        <span className='text-white'>Footer</span>
+    </footer>
+  )
+}
