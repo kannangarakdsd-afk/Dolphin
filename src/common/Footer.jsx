4b98ff318739f04d292e191800f3282324bfe510
kkdsavindu <kannangara.kdsd@gmail.com>
2026-01-10 11:39:53 +0530

    Footer update

--- src/common/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Footer() {
-  return (
-    <footer className='bg-[#0F2854]'>
-        <span className='text-white'>Footer</span>
-    </footer>
-  )
-}
