eb0c6e3f465998cd16c00bf11392b2490d3058ef
kkdsavindu <kannangara.kdsd@gmail.com>
2026-01-10 12:14:27 +0530

    Error

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,24 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './common/Header'
-import { Route, Routes } from 'react-router-dom'
-
-import MainLayout from './Layout/MainLayout'
-import HomePage from './Pages/HomePage'
-import GalleryPage from './Pages/GalleryPage'
+import { Route, Router } from 'react-router-dom'
+import Footer from './common/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <Routes>
-      <Route element={<MainLayout />}>
-       <Route path="/" element={<HomePage />} />
-       <Route path="/gallery" element={<GalleryPage />} />
-      </Route>
-    </Routes>
+    <div className="flex flex-col min-h-screen w-full">
+      
+      <Header className="w-full rounded-none" /> 
+      
+      <main className="flex-grow w-full">
+        <span className="text-black">Main Content</span>
+      </main>
+      
+      <Footer className="w-full rounded-none" />
+      
+    </div>
   )
 }
 
