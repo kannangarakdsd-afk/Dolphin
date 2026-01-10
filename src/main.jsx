5a47533aa12800f035de8d1b64ed1620a458bb73
kkdsavindu <kannangara.kdsd@gmail.com>
2026-01-10 15:01:02 +0530

    Changes applied

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-<<<<<<< HEAD
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-=======
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
@@ -17,5 +10,4 @@ createRoot(document.getElementById('root')).render(
       <App />
     </StrictMode>
   </BrowserRouter>
->>>>>>> 4def3c8436e1caa1949eac458c5cbe99ab1e0178
 )
